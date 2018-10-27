ee463babf7488f2c01a42445a73d5916ad67c516
Maciej <maciej.falbogowski@gmail.com>
2018-10-27 22:54:34 +0200

    Addedd readme.md

--- img_to_ascii.py
+++ img_to_ascii.py
@@ -1,24 +1,35 @@
 import numpy as np
 from PIL import Image
+import sys
 
-IMAGES_FOLDER = 'Images/'
-IMAGE_FILE = 'sunflower.png'
-ASCII_FILE = 'ascii.txt'
-ASCII_IMAGE_SIZE = (3 * 64, 64)
+INVERSED_SPECTRUM = '$@B%8&WM#\*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`\'. ' #from most black to most white
+FULL_SPECTRUM = inversed_spectrum[::-1] #same as above but from most white to most black
+SMALL_SPECTRUM = " .:-=+*#%@" #10 levels from most white to most black
 
-#from most black to most white
-inversed_spectrum = '$@B%8&WM#\*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`\'. '
-#from most white to most black
-full_spectrum = inversed_spectrum[::-1]
-small_spectrum = " .:-=+*#%@" #10 levels 
 
-ascii_spectrum = small_spectrum 
-bins = np.linspace(0, 256, len(ascii_spectrum))
+def image_to_ascii(image_file_path, width, height, ascii_spectrum):
+    #np.linspace(0,16,5) -> [0, 4, 8, 12, 16] 5 elements from 0 to 16 arranged linearly. You got the idea
+    bins = np.linspace(0, 256, len(ascii_spectrum)) 
+    #greyscale image (8-bit pixels, only black and white)
+    if (width,height) != (None, None):
+        grim = Image.open(image_file_path).resize((width,height)).convert('L')
+    else:
+        grim = Image.open(image_file_path).convert('L')
+    ascii_data = np.array([ascii_spectrum[x] for x in np.digitize(grim.getdata(), bins)]).reshape(grim.size[::-1]) #if because of row - column form of numpy ndarray
+    return '\n'.join([''.join(row) for row in ascii_data])
 
-image = Image.open(IMAGES_FOLDER + IMAGE_FILE)
-#greyscale image
-grim = image.resize(ASCII_IMAGE_SIZE).convert('L') #(8-bit pixels, black and white)
-ascii_data = np.array([ascii_spectrum[x] for x in np.digitize(grim.getdata(), bins)]).reshape(grim.size[::-1])
 
-with open(ASCII_FILE, 'w+') as f:
-    f.write('\n'.join([''.join(row) for row in ascii_data]))
+def main():
+    try:
+        _, image_file, width, height, *_ = *sys.argv, None, None
+    except ValueError:
+        print('Not enough arguments. Did you forget name of the file?\nTry: python image_to_ascii.py <path_to_image>')
+        raise SystemExit
+    output_file = image_file.split('.')[0] + '.txt'
+    result = image_to_ascii(image_file, width, height, SMALL_SPECTRUM) #You can choose one of spectrum above for different results. 'small_spectrum' looks the best in my opinion
+    with open(output_file, 'w+') as f:
+        f.write(result)
+        print('Saved in', image_file)
+
+if __name__ == '__main__':
+    main()
--- tiny_example/image_to_ascii.py
+++ tiny_example/image_to_ascii.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 ascii_spectrum = ' .:-=+*%@'
 bins = np.linspace(0, 256, len(ascii_spectrum))
-grim = Image.open('python-logo.png').resize((64,32)).convert('L')
+grim = Image.open('../python-logo.png').resize((64,32)).convert('L')
 ascii_data = np.array([ascii_spectrum[x-1] for x in np.digitize(grim.getdata(), bins)]).reshape(grim.size[::-1]) #if because of row - column form of numpy ndarray
 with open('result.txt', 'w+') as f:
     f.write('\n'.join([''.join(row) for row in ascii_data]))
